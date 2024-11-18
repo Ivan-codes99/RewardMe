6627d692bdaf82cb9f70c96a25a9ceec73cf9236
Ivan Figueroa <Ivanfiggomez@gmail.com>
2024-11-18 17:10:20 -0500

    Created roleMiddleware.js
    Checks if the user's role matches one of the allowed roles and handles both scenarios.

---
+++ backend/middlewares/roleMiddleware.js
@@ -0,0 +1,22 @@
+function roleMiddleware(allowedRoles) {
+    
+    
+    return (req, res, next) => {
+    // Check if the user role mateches allowed roles.    
+
+    if (!req.user) {
+        res.status(403).json({"msg": "Access denied."});
+    }
+    
+    else {
+        role = req.user.role
+        if (allowedRoles.includes(role)) {
+            next();
+        }
+        else {
+            res.status(403).json({"msg": "Access denied."});
+        }
+    }
+    
+    }
+}
